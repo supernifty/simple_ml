c02edf85baac65f5dd360a5b9269639d93e72042
Peter Georgeson <peter@supernifty.org>
2018-10-25 11:31:35 +1100

    use random forest; add iris dataset

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ module load Python/3.6.4-intel-2017.u2
 python -m venv ml-venv
 pip install -r requirements.txt
 ```
+
+## Usage
+```
+python ml.py --verbose --X X.tsv --y y.tsv
+```
--- ml.py
+++ ml.py
@@ -10,21 +10,23 @@ import logging
 import operator
 import sys
 
-
 import numpy as np
 
+import sklearn.ensemble
 import sklearn.linear_model
 import sklearn.model_selection
+import sklearn.tree
 
 def main(x_fn, y_fn, names_fn):
   logging.info('starting...')
 
   xs = []
   header = None
-  for row in csv.reader(open(x_fn, 'r'), delimiter='\t'):
+  for idx, row in enumerate(csv.reader(open(x_fn, 'r'), delimiter='\t')):
     if header is None:
       header = row
       continue
+    logging.debug('xs line %i', idx + 1)
     xs.append([float(x) for x in row])
 
   ys = []
@@ -35,17 +37,21 @@ def main(x_fn, y_fn, names_fn):
     if class_header is None:
       class_header = name
       continue
-    ys.append(int(name))
-    classes.add(int(name))
+    #ys.append(int(name))
+    #classes.add(int(name))
+    ys.append(name)
+    classes.add(name)
 
   xs = np.array(xs)
   ys = np.array(ys)
 
   # TODO names
+  print(xs)
+  print(ys)
 
   logging.info('predicting...')
   # learn
-  predictor = sklearn.linear_model.LogisticRegression(random_state=0, solver='liblinear', multi_class='auto')
+  #predictor = sklearn.linear_model.LogisticRegression(random_state=0, solver='liblinear', multi_class='auto')
 
   shuffler = sklearn.model_selection.StratifiedShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
 
@@ -53,26 +59,31 @@ def main(x_fn, y_fn, names_fn):
   coeffs = collections.defaultdict(list)
 
   for train_idx, test_idx in shuffler.split(xs, ys):
+    #print(train_idx, test_idx)
+    #predictor = sklearn.linear_model.LogisticRegression(random_state=0, solver='liblinear', multi_class='auto')
+    #predictor = sklearn.tree.DecisionTreeClassifier()
+    predictor = sklearn.ensemble.RandomForestClassifier()
     fit_predictor = predictor.fit(xs[train_idx], ys[train_idx])
     score = fit_predictor.score(xs[test_idx], ys[test_idx])
     scores.append(score)
-    for name, value in zip(header, fit_predictor.coef_[0]):
-      coeffs[name].append(value)
+    logging.debug('score: %.3f', score)
+    #for name, value in zip(header, fit_predictor.coef_[0]):
+    #  coeffs[name].append(value)
 
-  coeffs_summary = {}
-  for key in coeffs:
-    coeffs_summary[key] = sum(coeffs[key]) / len(coeffs[key])
+  #coeffs_summary = {}
+  #for key in coeffs:
+  #  coeffs_summary[key] = sum(coeffs[key]) / len(coeffs[key])
 
-  logging.info('mean score: {:.2f}'.format(sum(scores) / len(scores)))
-  for name, value in sorted(coeffs_summary.items(), key=operator.itemgetter(1)):
-    logging.info('coeff %s: %.2f', name, value)
+  logging.info('mean score: {:.3f}'.format(sum(scores) / len(scores)))
+  #for name, value in sorted(coeffs_summary.items(), key=operator.itemgetter(1)):
+  #  logging.info('coeff %s: %.2f', name, value)
   logging.info('done')
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Classify mutational signatures')
   parser.add_argument('--X', required=True, help='tsv of inputs')
   parser.add_argument('--y', required=True, help='labels')
-  parser.add_argument('--names', required=False, help='names of inputs')
+  parser.add_argument('--names', required=False, help='names of inputs NOT implemented')
   parser.add_argument('--verbose', action='store_true', help='more logging')
   args = parser.parse_args()
   if args.verbose:
